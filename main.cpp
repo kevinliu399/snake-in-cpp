9c6fe0b7cf64f261626d1678ac48523f65bf423b
kevinliu399 <kevinliu399@gmail.com>
2024-08-14 23:26:10 -0400

    end

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <raymath.h>
 #include <deque>
 using namespace std;
 
@@ -8,38 +9,86 @@ Color darkGreen = {43, 51, 24, 255};
 
 int cellSize = 30;
 int cellCount = 25;
+int offset = 75;
 
-class Snake {
+double lastUpdateTime = 0;
 
-public :
-    deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+bool eventTriggered(double interval)
+{
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+
+    return false;
+}
+
+bool ElementInDeque(Vector2 element, deque<Vector2> deque)
+{
+    for (unsigned int i = 0; i < deque.size(); i++)
+    {
+        if (Vector2Equals(element, deque[i]))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+class Snake
+{
+
+public:
+    deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+    Vector2 direction = Vector2{1, 0};
+    bool addSegment = false;
 
     void Draw()
     {
         for (unsigned int i = 0; i < body.size(); i++)
-        {   
+        {
             float x = body[i].x;
             float y = body[i].y;
-            Rectangle segment = Rectangle{x * cellSize, y * cellSize, (float) cellSize, (float) cellSize};
+            Rectangle segment = Rectangle{offset + x * cellSize, offset + y * cellSize, (float)cellSize, (float)cellSize};
             DrawRectangleRounded(segment, 0.5, 6, darkGreen);
         }
     }
 
+    void Update()
+    {
+        body.push_front(Vector2Add(body[0], direction));
+        if (addSegment)
+        {
+            addSegment = false;
+        } else {
+            body.pop_back();
+        }
+
+    }
+
+    void Reset() {
+        body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+        direction = Vector2{1, 0};
+    }
 };
 
-class Food {
+class Food
+{
 
 public:
     Vector2 position;
     Texture2D texture;
 
-    Food()
+    Food(deque<Vector2> snakeBody)
     {
         Image image = LoadImage("graphics/apple2.png");
         texture = LoadTextureFromImage(image);
         UnloadImage(image);
 
-        position = GenerateRandomPos();
+        position = GenerateRandomPos(snakeBody);
     }
 
     ~Food()
@@ -49,31 +98,132 @@ public:
 
     void Draw()
     {
-        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+        DrawTexture(texture, offset + position.x * cellSize, offset + position.y * cellSize, WHITE);
     }
 
-    Vector2 GenerateRandomPos() {
+    Vector2 GenerateRandomCell() {
         float x = GetRandomValue(0, cellCount - 1);
         float y = GetRandomValue(0, cellCount - 1);
         return Vector2{x, y};
     }
+
+    Vector2 GenerateRandomPos(deque<Vector2> snakeBody)
+    {
+        Vector2 position = GenerateRandomCell();
+        while(ElementInDeque(position, snakeBody))
+        {
+            position = GenerateRandomCell();
+        }
+        return position;
+    }
 };
 
-int main() {
+
+class Game
+{
+public:
+    Snake snake = Snake();
+    Food food = Food(snake.body);
+    bool running = true;
+    int score = 0;
+
+    void Draw()
+    {
+        food.Draw();
+        snake.Draw();
+    }
+
+    void Update()
+    {
+        if (running) {
+            snake.Update();
+            CheckCollisionWithFood();
+            CheckCollisionWithEdges();
+            CheckCollisionWithTail();
+        }
+    }
+
+    void CheckCollisionWithFood() {
+        if (Vector2Equals(snake.body[0], food.position))
+        {
+            food.position = food.GenerateRandomPos(snake.body);
+            snake.addSegment = true;
+            score ++;
+        }
+    }
+
+    void CheckCollisionWithEdges() {
+        if (snake.body[0].x == cellCount || snake.body[0].x == -1) {
+            GameOver();
+        }
+        if (snake.body[0].y == cellCount || snake.body[0].y == -1) {
+            GameOver();
+        }
+    }
+
+    void GameOver() {
+        snake.Reset();
+        food.position = food.GenerateRandomPos(snake.body);
+        running = false;
+        score = 0;
+    }
+
+    void CheckCollisionWithTail() {
+        deque<Vector2> headlessBody = snake.body;
+        headlessBody.pop_front();
+
+        if (ElementInDeque(snake.body[0], headlessBody)) {
+            GameOver();
+        }
+    }
+};
+
+int main()
+{
     cout << "Starting the game" << endl;
-    InitWindow(cellSize * cellCount, cellSize * cellCount, "Snake Game");
+    InitWindow(2 * offset + cellSize * cellCount, 2 * offset + cellSize * cellCount, "Snake Game");
     SetTargetFPS(60);
 
-    Food food = Food();
-    Snake snake = Snake();
+    Game game = Game();
 
-    while (WindowShouldClose() == false) 
+    while (WindowShouldClose() == false)
     {
         BeginDrawing();
 
+        if (eventTriggered(0.2))
+        {
+            game.Update();
+        }
+
+        if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1)
+        {
+            game.snake.direction = Vector2{0, -1};
+            game.running = true;
+        }
+
+        if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1)
+        {
+            game.snake.direction = Vector2{0, 1};
+            game.running = true;
+        }
+
+        if (IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1)
+        {
+            game.snake.direction = Vector2{-1, 0};
+            game.running = true;
+        }
+
+        if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1)
+        {
+            game.snake.direction = Vector2{1, 0};
+            game.running = true;
+        }
+
         ClearBackground(green);
-        food.Draw();
-        snake.Draw();
+        DrawRectangleLinesEx(Rectangle{(float)offset-5, (float)offset-5, (float)cellSize * cellCount+10, (float)cellSize * cellCount+10}, 5, darkGreen);
+        DrawText("Snake Game", offset - 5, 20, 30, darkGreen);
+        DrawText(TextFormat("Score: %i", game.score), offset + cellSize * cellCount - 100, 20, 30, darkGreen);
+        game.Draw();
 
         EndDrawing();
     }
